f1a4ebf91902c9b388816f066dafc1072a2eb36e
Dominowa <babicyura@gmail.com>
2023-07-20 14:17:10 +0300

    1-4 task

---
+++ src/js/task-02.js
@@ -0,0 +1,15 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const ingredientsList = document.getElementById("ingredients");
+ingredients.forEach((ingredient) => {
+  const liElement = document.createElement("li");
+  liElement.textContent = ingredient;
+  liElement.classList.add("item");
+  ingredientsList.appendChild(liElement);
+});
