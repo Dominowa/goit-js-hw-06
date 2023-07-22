c374f4a0eabc3c738d1e959c851c2382748d8a87
Dominowa <babicyura@gmail.com>
2023-07-22 13:18:57 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,16 +1,33 @@
 const loginForm = document.querySelector(".login-form");
+
 loginForm.addEventListener("submit", function (event) {
   event.preventDefault();
+
   const formData = new FormData(event.target);
 
   const loginData = {};
   formData.forEach((value, name) => {
     loginData[name] = value;
   });
+
   if (loginData.email.trim() === "" || loginData.password.trim() === "") {
     alert("Please fill in all fields.");
   } else {
     console.log(loginData);
     loginForm.reset();
+    // fetch('/api/login', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(loginData),
+    // })
+    // .then((response) => response.json())
+    // .then((data) => {
+    //   console.log(data);
+    // })
+    // .catch((error) => {
+    //   console.error('Error:', error);
+    // });
   }
 });
