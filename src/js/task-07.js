7b6c3c05864d72b37fff6ee592caab7563cf075f
Dominowa <babicyura@gmail.com>
2023-07-24 12:36:02 +0300

    up

--- src/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const fontSizeControl = document.getElementById("font-size-control");
-const textElement = document.getElementById("text");
-fontSizeControl.addEventListener("input", () => {
-  const fontSizeValue = fontSizeControl.value;
-  textElement.style.fontSize = `${fontSizeValue}px`;
-});
