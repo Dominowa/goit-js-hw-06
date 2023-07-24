b7cdcb33febf13d056cbe3755e1c9f353c3c20cb
Dominowa <babicyura@gmail.com>
2023-07-24 14:19:21 +0300

    up

--- js/task-08.js
+++ js/task-08.js
@@ -2,18 +2,18 @@ const formRef = document.querySelector(".login-form");
 
 const formSubmitHandler = (event) => {
   event.preventDefault();
-  const formData = new FormData(event.target);
-  const emailValue = formData.get("email");
-  const passwordValue = formData.get("password");
+
+  const {
+    email: { value: emailValue },
+    password: { value: passwordValue },
+  } = event.currentTarget.elements;
 
   if (emailValue.trim() === "" || passwordValue.trim() === "") {
     alert("All the fields should be completed ;-)");
     return;
   }
-
-  console.log("Login:", emailValue);
-  console.log("Password:", passwordValue);
-
+  const dataForm = { name: emailValue, password: passwordValue };
+  console.log(dataForm);
   formRef.reset();
 };
 
