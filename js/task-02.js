7f1341f4115d459cf8462b56866beb97f2b9fd48
Dominowa <babicyura@gmail.com>
2023-07-25 11:56:32 +0300

    up

--- js/task-02.js
+++ js/task-02.js
@@ -1,39 +1,36 @@
-const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
-];
+// const ingredients = [
+//   "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",
+// ];
+// const ingredientsList = document.getElementById("ingredients");
+
+// function createIngredientsList() {
+//   const itemsHTML = ingredients
+//     .map((ingredient) => `<li class="item">${ingredient}</li>`)
+//     .join("");
+
+//   ingredientsList.insertAdjacentHTML("beforeend", itemsHTML);
+// }
+
+// createIngredientsList();
+
 const ingredientsList = document.getElementById("ingredients");
 
 function createIngredientsList() {
-  const itemsHTML = ingredients
-    .map((ingredient) => `<li class="item">${ingredient}</li>`)
-    .join("");
+  const fragment = document.createDocumentFragment();
 
-  ingredientsList.insertAdjacentHTML("beforeend", itemsHTML);
+  ingredients.forEach((ingredient) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = ingredient;
+    listItem.classList.add("item");
+    fragment.appendChild(listItem);
+  });
+
+  ingredientsList.appendChild(fragment);
 }
 
 createIngredientsList();
-
-//     const ingredientsList = document.getElementById("ingredients");
-
-//     function createIngredientsList() {
-//       const fragment = document.createDocumentFragment();
-
-//       ingredients.forEach((ingredient) => {
-//         const listItem = document.createElement("li");
-//         listItem.textContent = ingredient;
-//         listItem.classList.add("item");
-//         fragment.appendChild(listItem);
-//       });
-
-//       ingredientsList.appendChild(fragment);
-//     }
-
-//     createIngredientsList();
-//   </script>
-// </body>
-// </html>
