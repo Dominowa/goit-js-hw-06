52a3769d27a48749d0fb907325b2b62cc686fc12
Dominowa <babicyura@gmail.com>
2023-07-25 11:58:48 +0300

    up

--- js/task-02.js
+++ js/task-02.js
@@ -1,23 +1,3 @@
-// const ingredients = [
-//   "Potatoes",
-//   "Mushrooms",
-//   "Garlic",
-//   "Tomatos",
-//   "Herbs",
-//   "Condiments",
-// ];
-// const ingredientsList = document.getElementById("ingredients");
-
-// function createIngredientsList() {
-//   const itemsHTML = ingredients
-//     .map((ingredient) => `<li class="item">${ingredient}</li>`)
-//     .join("");
-
-//   ingredientsList.insertAdjacentHTML("beforeend", itemsHTML);
-// }
-
-// createIngredientsList();
-
 const ingredientsList = document.getElementById("ingredients");
 
 function createIngredientsList() {
